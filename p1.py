c71005720dcd8398a5bf93f56a8f245da43eb89e
shayestasana <108970819+shayestasana@users.noreply.github.com>
2022-07-23 12:10:37 +0530

    h

---
+++ p1.py
@@ -0,0 +1,12 @@
+from turtle import * # turtle is visual programming
+forward(200) # move forward 200 units
+left(90)  # turn left 90 degrees
+forward(100)
+left(90)
+forward(200)
+left(90)
+right(60)
+forward(100)
+left(360)
+
+mainloop() #stops the window from closing
