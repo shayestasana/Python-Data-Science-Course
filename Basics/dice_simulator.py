b686b1d8ef6874ab442117cb7e1863920e6529f3
shayestasana <108970819+shayestasana@users.noreply.github.com>
2022-08-03 11:56:57 +0530

    h

--- Basics/dice_simulator.py
+++ Basics/dice_simulator.py
@@ -9,17 +9,13 @@ while True:
     input("Press enter to Roll a Dice")
     out=randint(1,6)
     print(f'🎲 => {dice[out-1]}')
-    n=0
-    while(n<3):
-        if out == 6:
-            win_count += 1
-        elif out == 1:
-            lose_count += 1
-        n=n+1
-
-
+    if out == 6:
+        win_count += 1
+    elif out == 1:
+        lose_count += 1
+    
     if win_count==3:
-        print("You Win 🤴")
+        print("You Win 👑")
         break
     elif lose_count ==3:
         print("You Lose 😞")
